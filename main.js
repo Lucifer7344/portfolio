f992b26f0dc39fa8ff848f296e91491da14d17aa
lucifer <luciferthedevil7344@gmail.com>
2023-10-12 16:50:00 +0530

    stylesheet

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new typed(".text",{
+var typed= new typed(".Text",{
     String:["Frontend Developer", "Youtuber","Web Developer","Ethical Hacker"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 })
