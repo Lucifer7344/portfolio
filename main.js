56cf3d1dd158fdc45aa9e9551adc09bf61dd3ee5
lucifer7344 <workwithharshitkatiyar@gmail.com>
2023-11-05 13:20:43 +0530

    new

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new typed(".Text", {
-    String: ["Frontend Developer","Web Developer","Ethical Hacker"],
+var typed= new Typed('.Text', {
+    strings: ['Frontend Developer','Web Developer','Ethical Hacker'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop: true,
+    loop:true,
 })
